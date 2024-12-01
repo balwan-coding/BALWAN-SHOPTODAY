61417bde0dd9883fbf51924fd5271dc5fbcd2c82
balwan coding <balwansinghrajput1708@gmail.com>
2024-12-01 19:31:44 +0530

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -12,14 +12,11 @@ function Navbar({ cartCount, user, setUser }) {
 
   return (
     <nav className="flex items-center justify-between px-6 py-2 bg-white">
-      {/* Logo Section */}
       <div className="w-12 sm:w-16 md:w-16 aspect-square">
         <img className="object-cover w-full h-full" src={logo} alt="logo" />
       </div>
 
-      {/* Action Buttons */}
       <div className="flex items-center">
-        {/* Logout Button (Visible only if user is logged in) */}
         {user ? (
           <button
             className="px-4 py-2 font-bold text-white bg-indigo-600 rounded-md hover:bg-indigo-500"
@@ -28,7 +25,6 @@ function Navbar({ cartCount, user, setUser }) {
             Logout
           </button>
         ) : (
-          // Login/SignUp Button (Visible only if user is not logged in)
           <Link
             className="px-4 py-2 font-bold text-white bg-blue-600 rounded-md hover:bg-blue-500"
             to="/LoginPage"
@@ -37,7 +33,6 @@ function Navbar({ cartCount, user, setUser }) {
           </Link>
         )}
 
-        {/* Cart Button */}
         <Link to="/CartPage" className="flex items-center">
           <span className="relative flex text-2xl font-bold text-orange-600 left-11">
             {cartCount}
